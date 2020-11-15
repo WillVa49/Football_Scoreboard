d8e52b1b8d1577c1b57d209eacce8049e6956975
Wilfrido Varela <wilfrido.varela@my.tccd.edu>
2020-11-15 03:45:35 +0000

    Added menu. Trying to figure out why nothing pops up for the second team.

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 #include <iostream>
 using namespace std;
 
-class Team
+class Team1
 {
   private:
     string Name;
@@ -14,12 +14,12 @@ class Team
     int score;
     int TimeoutCount;
   public:
-    Team()
+    Team1()
     {
-      Name = "Will V.";
+      Name = "DefaultName";
       CoachName = "Dr.T";
       HomeCity = "DefaultHomeCity";
-      HomeStatus = false; //Visitor = false, Home = True
+      HomeStatus = true; //Visitor = false, Home = True
       score = 0;
       TimeoutCount = 0;
     }
@@ -36,24 +36,56 @@ class Team
     int getScore() const {return score;}
     int getTimoutCount() const {return TimeoutCount;} 
 };
+class Team2
+{
+  private:
+    string Name;
+    string CoachName;
+    string visitorCity;
+    bool visitorStatus;
+    int score;
+    int TimeoutCount;
+  public:
+    Team2()
+    {
+      Name = "DefaultName";
+      CoachName = "Dr.T";
+      visitorCity = "DefaultHomeCity";
+      visitorStatus = false; //Visitor = false, Home = True
+      score = 0;
+      TimeoutCount = 0;
+    }
+    void setName(string n) {Name = n;}
+    void setCoachName(string c) {CoachName = c;}
+    void setVisitorCity (string hc) {visitorCity = hc;}
+    void setVisitorStatus (bool hs) {visitorStatus = hs;}
+    void setScore (int s) {score = s;}
+    void setTimeoutCount (int t) {TimeoutCount = t;}
+    string getName() const {return Name;}
+    string getCoachName() const {return CoachName;}
+    string getVisitorCity() const {return visitorCity;}
+    bool getVisitorStatus() const {return visitorStatus;}
+    int getScore() const {return score;}
+    int getTimoutCount() const {return TimeoutCount;} 
+};
 
 class Scoreboard
 {
   private:
     int quarter;
-    Team home;
-    Team visitor;
+    Team1 home;
+    Team2 visitor;
   public:
     Scoreboard()
     {
       quarter = 1;
     }
     void setQuarter (int q) {quarter = q;}
-    void setHome (Team hSet) {home = hSet;}
-    void setVisitor (Team vSet) {visitor = vSet;}
+    void setHome (Team1 hSet) {home = hSet;}
+    void setVisitor (Team2 vSet) {visitor = vSet;}
     int getQuarter() const {return quarter;}
-    Team getHome() const {return home;}
-    Team getVisitor() const {return visitor;}
+    Team1 getHome() const {return home;}
+    Team2 getVisitor() const {return visitor;}
     void ShowScoreboard()
     {
       string color = "";
@@ -69,38 +101,75 @@ class Scoreboard
 
 int main() 
 {
-  Team();
+  Team1();
+  Team2();
   Scoreboard s;
-  Team tOne;
-  Team tTwo;
+  Team1 tOne;
+  Team2 tTwo;
   string newName = "";
   string userChoice = "";
   int newScore = 0;
+  int newQuarter = 0;
 
   tOne.setHomeStatus(true);
 
   s.setHome(tOne);
   s.setVisitor(tTwo);
 
-//***************************************//
-do
-{
-  system("clear");
-  s.ShowScoreboard();
-  cout << "A = Update Home Team name: " << endl;
-  cout << "B = Update Visitor Team name: " << endl;
-  cout << "C = Home Team Score: " << endl;
-  cout << "D = Visitor Team Score: " << endl;
-  cout << "F = Update Qtr: " << endl;
-  cin >> userChoice;
-
-  if (userChoice == "A" || userChoice == "a")
+  do
   {
-    cout << "Please enter a new name for the home team: ";
-    cin >> newName;
-  }
- 
+    system("clear");
+    s.ShowScoreboard();
+    cout << "A = Update Home Team name: " << endl;
+    cout << "B = Update Visitor Team name: " << endl;
+    cout << "C = Home Team Score: " << endl;
+    cout << "D = Visitor Team Score: " << endl;
+    cout << "F = Update Qtr: " << endl;
+    cout << "E = exit: ;" << endl;
+    cin >> userChoice;
 
+    if (userChoice == "A" || userChoice == "a")
+    {
+      cout << "\nPlease enter a new name for the home team: ";
+      cin >> newName;
+      tOne.setName(newName);
+      s.ShowScoreboard();
+    }
+    else if (userChoice == "B" || userChoice == "b")
+    {
+      cout << "\nPlease enter a new name for the visitor team: " << endl;
+      cin >> newName;
+      tTwo.setName(newName);
+      s.ShowScoreboard();
+    }
+    else if (userChoice == "C" || userChoice == "c")
+    {
+      cout << "\nPlease enter a new score for the home team: " << endl;
+      cin >> newScore;
+      tOne.setScore(newScore);
+    }
+    else if (userChoice == "D" || userChoice == "d")
+    {
+      cout << "\nPlease enter a new score for the home team: " << endl;
+      cin >> newScore;
+      tTwo.setScore(newScore);
+    }
+    else if (userChoice == "F" || userChoice == "f")
+    {
+      cout << "\nPlease enter what quarter they are in: " << endl;
+      cin >> newQuarter;
+    }
+    else if (userChoice == "E" || userChoice == "e")
+    {
+      cout << "Exit chosen." << endl;
+    }
+    else
+    {
+      cout << "\nInvalid input, try again." << endl;
+    }
 
-return 0;
+    s.setHome(tOne);
+  }while (userChoice != "E" && userChoice != "e");
+  
+  return 0;
 }
